dd7a72e7531e9969767c0ec31c90d8bc15248bd8
almeidacostapedrohenrique-create <almeidacostapedrohenrique@gmail.com>
2025-10-20 10:47:21 -0300

    Add files via upload

---
+++ PedroCosta/config_em_JS.js
@@ -0,0 +1,10 @@
+const click = document.getElementById("mapa");
+const botoes = document.querySelectorAll(".op")
+click.addEventListener("click", () => {
+    alert("clicado");
+});
+botoes.forEach(botao =>{
+    botao.addEventListener("mouseover", (evento) =>{
+        
+    });
+});
