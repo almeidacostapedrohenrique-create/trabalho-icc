ca008f634c1ca1282341bc74db7114d382001766
almeidacostapedrohenrique-create <almeidacostapedrohenrique@gmail.com>
2025-11-10 16:26:20 -0300

    Add files via upload

--- PedroCosta/config_em_JS.js
+++ PedroCosta/config_em_JS.js
@@ -1,40 +1,7 @@
-const click = document.getElementById("mapa");
-const click2 = document.getElementById("bosses");
-const click3 = document.getElementById("Amuleto");
-const click4= document.getElementById("Historia");
-const click5 = document.getElementById("poderzin");
-const click6 = document.getElementById("npc");
-
-
 const botoes = document.querySelectorAll(".op");
 
 
 
-
-
-click.addEventListener("click", () => {
-    window.location.href = "C:/Users/labcaxias/Desktop/PEDRO/trabalho-icc-main/PedroCosta/MAPA.html";
-});
-
-click2.addEventListener("click", () => {
-    window.location.href = "C:/Users/labcaxias/Desktop/PEDRO/trabalho-icc-main/PedroCosta/Bosses.html";
-});
-
-click3.addEventListener("click", () => {
-    window.location.href = "C:/Users/labcaxias/Desktop/PEDRO/trabalho-icc-main/PedroCosta/MAPA.html";
-});
-
-click4.addEventListener("click", () => {
-    window.location.href = "C:/Users/labcaxias/Desktop/PEDRO/trabalho-icc-main/PedroCosta/MAPA.html";
-});
-
-click5.addEventListener("click", () => {
-    window.location.href = "C:/Users/labcaxias/Desktop/PEDRO/trabalho-icc-main/PedroCosta/MAPA.html";
-});
-
-click6.addEventListener("click", () => {
-    window.location.href = "C:/Users/labcaxias/Desktop/PEDRO/trabalho-icc-main/PedroCosta/MAPA.html";
-});
 
 botoes.forEach(botao => {   
    
